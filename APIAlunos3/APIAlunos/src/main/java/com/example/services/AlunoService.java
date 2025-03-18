454d2ed082823756ee2f4ae8932b6fb4d53673b3
naamacosta <naamacosta@gmail.com>
2025-03-17 21:50:40 -0300

    API-4

--- APIAlunos3/APIAlunos/src/main/java/com/example/services/AlunoService.java
+++ APIAlunos3/APIAlunos/src/main/java/com/example/services/AlunoService.java
@@ -6,8 +6,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AlunoService {
@@ -18,7 +21,7 @@ public class AlunoService {
 
     public AlunoService() throws IOException{
         this.objectMapper = new ObjectMapper();
-        this.alunoModels = getAlunos();
+        this.alunoModels = carregarAlunos();
     }
 
     public ResponseEntity<List<AlunoModel>> getAlunos() {
@@ -33,7 +36,46 @@ public class AlunoService {
     }
 
     public ResponseEntity<AlunoModel> updateAluno(long id, AlunoModel aluno) throws IOException{
+        Optional<AlunoModel> optionalAlunoModel = alunoModels.stream().filter(a -> a.getId() == id).findFirst();
 
+        if(optionalAlunoModel.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        AlunoModel alunoAtualizar = optionalAlunoModel.get();
+        alunoAtualizar.setNome(aluno.getNome());
+        alunoAtualizar.setEmail(aluno.getEmail());
+        alunoAtualizar.setDataNascimento(aluno.getDataNascimento());
+
+        salvarAlunos();
+        return ResponseEntity.ok(alunoAtualizar);
+
+    }
+
+    public ResponseEntity<AlunoModel> removeAluno (long id) throws IOException{
+        Optional<AlunoModel> optionalAlunoModel = alunoModels.stream().filter(a -> a.getId() == id).findFirst();
+
+        if(optionalAlunoModel.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        alunoModels.remove((optionalAlunoModel.get()));
+        salvarAlunos();
+        return ResponseEntity.ok(optionalAlunoModel.get());
+    }
+
+    private List<AlunoModel> carregarAlunos() throws IOException{
+        File file = new File(FILE_PATH);
+        if(file.exists()){
+            return objectMapper.readValue(file, objectMapper.getTypeFactory().constructCollectionType(List.class, AlunoModel.class));
+
+        }else{
+            return new ArrayList<>();
+        }
+    }
+
+    private void salvarAlunos() throws IOException{
+        objectMapper.writeValue(new File(FILE_PATH), alunoModels);
     }
 
 }
